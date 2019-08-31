14ea673e023aede74c511dd0c8d5624dede23c72
maiko05 <maikorto5@icloud.com>
2019-08-31 10:59:30 +0900

    rule削除機能追加

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,6 +1,6 @@
 class RulesController < ApplicationController
   protect_from_forgery with: :exception
-  before_action :authenticate_user!,only: [:create, :new]
+  before_action :authenticate_user!,only: [:create, :new,]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
@@ -23,6 +23,18 @@ class RulesController < ApplicationController
       redirect_to controller: :rules, action: :index
     else
       redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def edit
+    @rule = Rule.find(params[:id])
+  end
+
+  def destroy
+    @rule = Rule.find(params[:id])
+    if rule.user_id == current_user.id
+    @rule.destroy
+    redirect_to action: :index
     end
   end
 
